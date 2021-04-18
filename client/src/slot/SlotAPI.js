bf0ed9aadd1b3c49081c864ea16ef71741cd8263
chiaatt <chia.attard@gmail.com>
2021-04-18 13:27:38 +0200

    Renamed authHeader
    Renamed authHeader to AuthHeader

--- client/src/slot/SlotAPI.js
+++ client/src/slot/SlotAPI.js
@@ -1,7 +1,7 @@
 /*
 * Spin
 */
-import { authHeader } from '../_helpers/authHeader';
+import { authHeader } from '../_helpers/AuthHeader';
 
 export async function slot() {
     const response = await fetch(`/spins`, {
